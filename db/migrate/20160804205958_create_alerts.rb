35ca95fdae7c2b6c743f9881e122745fd2e26d9a
Maxime 'biximilien' Gauthier <maxime.gauthier88@gmail.com>
2016-08-05 15:49:29 -0400

    Basic locales for current messages

--- db/migrate/20160804205958_create_alerts.rb
+++ db/migrate/20160804205958_create_alerts.rb
@@ -2,15 +2,23 @@ class CreateAlerts < ActiveRecord::Migration[5.0]
   def change
     create_table :alerts do |t|
 
+      # Who triggered it
       t.integer :source_id
       t.string  :source_type
       t.string  :source_url
 
+      # Who receives the alert
       t.integer :target_id
       t.string  :target_type
       t.string  :target_url
 
+      # Displayed message
       t.string  :message
+
+      # (Optional) Associated ressource
+      t.integer :object_id
+      t.string  :object_type
+      t.string  :object_url
 
       t.timestamps
     end
