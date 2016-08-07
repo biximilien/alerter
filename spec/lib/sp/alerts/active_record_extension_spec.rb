9e77ee9a837f75d5bcd339f6ed5e90850502e4ed
Maxime 'biximilien' Gauthier <maxime.gauthier88@gmail.com>
2016-08-07 17:15:23 -0400

    Test active record extensions integration

--- spec/lib/sp/alerts/active_record_extension_spec.rb
+++ spec/lib/sp/alerts/active_record_extension_spec.rb
@@ -1,9 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe SP::Alerts::ActiveRecordExtension do
+  describe ActiveRecord::Base do
+    it "includes the 'ActiveRecordExtension' module" do
+      expect(ActiveRecord::Base.included_modules).to include(SP::Alerts::ActiveRecordExtension)
+    end
+
+    it "has the 'alertable' class method" do
+      expect(ActiveRecord::Base).to respond_to(:alertable)
+    end
+  end
+
   describe ".alertable" do
+    before(:all) { class RecordMock < ActiveRecord::Base; end }
+    before(:all) { class ModelMock; include SP::Alerts::ActiveRecordExtension; end }
+
+    it "includes the 'Alertable' module" do
+      RecordMock.alertable
+      expect(RecordMock.included_modules).to include(SP::Alerts::Alertable)
+      expect(ActiveRecord::Base.included_modules).not_to include(SP::Alerts::Alertable)
+    end
+
     it "allows resource to be alerted" do
-      skip
+      ModelMock.alertable
+      expect(ModelMock.new).to respond_to(:alert)
+      expect(ModelMock).not_to respond_to(:alert)
     end
   end
 end
