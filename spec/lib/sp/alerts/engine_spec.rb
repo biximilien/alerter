bb83c74834a6723d70c6014c93f125a7df7b5dfd
Maxime 'biximilien' Gauthier <maxime.gauthier88@gmail.com>
2016-08-10 16:15:59 -0400

    Engine specs

--- spec/lib/sp/alerts/engine_spec.rb
+++ spec/lib/sp/alerts/engine_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe SP::Alerts::Engine do
-
+  it "isolates 'SP::Alerts' namespace" do
+    expect(SP::Alerts::Engine).to respond_to :isolate_namespace
+  end
 end
